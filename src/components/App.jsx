6e9635556d2e14512a157ff9a7186cf83f947753
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-28 14:49:10 +0300

    Create routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from 'pages/HomePage';
+import MoviesPage from 'pages/MoviesPage';
+import MovieDetailsPage from 'pages/MovieDetailsPage';
+import { Layout } from "./Layout";
+
 export const App = () => {
   return (
-    <div>Hello</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+      </Route>
+    </Routes>
   );
 };
