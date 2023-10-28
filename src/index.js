d0abf6f8e0c9b8ee337045befe0ae84354e8ec97
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-28 13:30:52 +0300

    Starting refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/goit-react-hw-05-movies">
+        <App />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
